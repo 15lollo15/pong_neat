fa1ac80546141532a58edd21521d3186a14e7e33
15lollo15 <35702138+15lollo15@users.noreply.github.com>
2023-11-23 06:26:47 +0100

    update import from lib

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ class Game:
                     only_best = False
 
     def run(self, pop: Population):
+        print(len(pop.species))
         global only_best, frame_rate
         nn_g = pg.sprite.GroupSingle()
         print('num species: ' + str(len(pop.species)))
@@ -127,18 +128,12 @@ class Game:
             if len(self.balls_g.sprites()) == 0:
                 self.is_running = False
 
-        max_f = 0
-        max_b = 0
         for i, b in enumerate(self.balls):
-            if b.count_bounce > max_b:
-                max_b = b.count_bounce
             pop.agents[i].fitness = 2 ** (b.count_bounce + .00001)
             if b.distance < settings.SCREEN_SIZE[1]:
                 prop = 1 - (b.distance / settings.SCREEN_SIZE[1])
                 pop.agents[i].fitness *= 1 + (2 * prop)
             pop.agents[i].fitness /= pop.agents[i].brain.calculate_weight()
-            if pop.agents[i].fitness > max_f:
-                max_f = pop.agents[i].fitness
 
 
 
@@ -150,5 +145,5 @@ def fitness(pop:Population):
 if __name__ == '__main__':
     pg.init()
     pg.display.set_mode(settings.SCREEN_SIZE)
-    input()
+    #input()
     population.evolve(fitness)
